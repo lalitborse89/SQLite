3797d9ee5114342356b4f49bec70d695d4df0b16
lalitborse89 <lalitborse89@gmail.com>
2016-03-18 22:16:22 -0400

    SQLite DataBase

---
+++ sqlitedb/src/main/java/com/Androidcode/sqlitedb/Student.java
@@ -0,0 +1,23 @@
+package com.instinctcoder.sqlitedb;
+
+import android.content.Context;
+
+public class Student {
+    // Labels table name
+    public static final String TABLE = "Student";
+
+    // Labels Table Columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_name = "name";
+    public static final String KEY_email = "email";
+    public static final String KEY_pass = "pass";
+
+    public int student_ID;
+    public String name;
+    public String email;
+    public String pass;
+
+
+
+
+}
